24a43b8c43ff1a280ebce8980481d2553cfd5c80
clxmente <solorioeclem164@gmail.com>
2022-04-09 14:31:32 -0700

    blur-sm bg card

--- components/Project.jsx
+++ components/Project.jsx
@@ -4,7 +4,7 @@ function Project(props) {
   return (
     <div className="group relative">
       <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-t from-purple-400 to-sky-400 opacity-0 transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
-      <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-t from-purple-400 to-sky-400 opacity-0 transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
+      <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-t from-purple-400 to-sky-400 opacity-0 blur-sm transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
       <a href={props.url}>
         <button className="relative w-full rounded-lg border border-gray-700 bg-gray-900 px-7 py-6 transition-all duration-700 ease-in-out group-hover:bg-slate-800">
           <span className="mb-3 block text-left font-semibold text-gray-100">
