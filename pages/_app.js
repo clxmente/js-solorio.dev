18f427e3c9291a9a228887b7275bf6ec602b1493
clxmente <solorioeclem164@gmail.com>
2022-04-11 13:56:33 -0700

    ganalytics package

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,11 @@
 import "../styles/globals.css";
-import Script from "next/script";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <Script
-        strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-12ETQSZR5W"
-      />
-      <Script id="ganalytics">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-12ETQSZR5W');
-          `}
-      </Script>
+      <GoogleAnalytics measurementId="G-12ETQSZR5W" />
       <Component {...pageProps} />;
     </>
   );
