659997a5f25bae5e7107a64fa3a35381befa60c2
clxmente <solorioeclem164@gmail.com>
2022-05-14 21:18:18 -0700

    redirects shoutout jack

--- next.config.js
+++ next.config.js
@@ -4,4 +4,28 @@ module.exports = {
   images: {
     domains: ["github.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/clxmente",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/clementesolorio/",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/clxmente/",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/clxmente",
+        permanent: true,
+      },
+    ];
+  },
 };
