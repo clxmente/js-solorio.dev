4879d7f84613084980ed99ad846b6e425ab814e9
clxmente <solorioeclem164@gmail.com>
2022-08-31 11:27:18 -0700

    resize issue fix

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,9 +1,14 @@
 function Project(props) {
   return (
-    <div className="group relative">
+    <div className="group relative flex">
       <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-t from-purple-400 to-sky-400 opacity-0 transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
       <div className="absolute -inset-0.5 rounded-lg bg-gradient-to-t from-purple-400 to-sky-400 opacity-0 blur-sm transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
-      <a href={props.url} target="_blank" rel="noopener noreferrer">
+      <a
+        href={props.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex"
+      >
         <button className="relative w-full rounded-lg border border-gray-700 bg-gray-900 px-7 py-6 transition-all duration-700 ease-in-out group-hover:bg-slate-800">
           <span className="mb-3 block text-left font-semibold text-gray-100">
             {props.title}
